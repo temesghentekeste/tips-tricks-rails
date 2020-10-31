7986d51595e3826e341ff090fc13806a57fd122e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-01 00:49:02 +0300

    Add scopes and class method to get cheap and expensive products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :brand
+
+  scope :expensive_products, -> { where(price: 50.01..Float::INFINITY).order(price: :desc)}
+  # Ex:- scope :active, -> {where(:active => true)}
+  def self.cheap_products
+    where(price: 0..50).order(price: :asc)
+  end
 end
